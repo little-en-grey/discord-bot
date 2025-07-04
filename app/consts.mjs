3092996264928960ad39c0ab0ef24276d219d13c
little-en-grey <little.en.grey@gmail.com>
2025-07-04 13:54:41 +0900

    本番環境用

--- app/consts.mjs
+++ app/consts.mjs
@@ -1,36 +1,32 @@
-// ------------------
-//      研究室用
-// ------------------
-
 // サーバーID
-export const GUILD_ID = "981489053134376960";
+export const GUILD_ID = "1341629229254840382";
 
 // スレッド作成対象チャンネルID
-export const PRIVATE_CH_ID = "1339812758719299654";     // プラベ
-export const OPEN_CH_ID = "1339812684962463764";        // オープン
-export const BATTLE_CH_ID = "1339812859164491777";      // 対抗戦
-export const ROTATION_CH_ID = "1338731785114222624";    // 定例プラベ
+export const PRIVATE_CH_ID = "1344960889316577321";     // プラベ
+export const OPEN_CH_ID = "1344960914838786090";        // オープン
+export const BATTLE_CH_ID = "1352495429530423346";      // 対抗戦
+export const ROTATION_CH_ID = "1344986714174918747";    // 定例プラベ
 
-export const PROFILE_CH_ID = "1386926479522271244";     // 自己紹介
+export const PROFILE_CH_ID = "1344953725864640562";     // 自己紹介
 
 // スレッド作成対象メンション
-export const PRIVATE_MENTION_ID = "1337426629877829774";   // プラベ
-export const OPEN_MENTION_ID = "1339816632142528562";      // オープン
-export const BATTLE_MENTION_ID = "1339816697460424735";    // 対抗戦
-export const LATE_NIGHT_MENTION_ID = "1355561690586026315";// 深夜
+export const PRIVATE_MENTION_ID = "1341629229262966834";   // プラベ
+export const OPEN_MENTION_ID = "1341629229254840391";      // オープン
+export const BATTLE_MENTION_ID = "1344956434697158747";    // 対抗戦
+export const LATE_NIGHT_MENTION_ID = "1344956335619444817";// 深夜
 
 // リアクション用カスタム絵文字
 export const REACTION_EMOJI = [
-  { name: "join_1", id: "1339815279533883473" },
-  { name: "join_2", id: "1339815281475850272" },
-  { name: "join_3", id: "1339815277583405126" },
+  { name: "join_1", id: "1347242670837469275" },
+  { name: "join_2", id: "1347242678873620560" },
+  { name: "join_3", id: "1347242685446226070" },
 ];
 // クローズ用絵文字
-export const CLOSE_EMOJI = { name: "shime", id: "1338747018595667968" };
+export const CLOSE_EMOJI = { name: "shime", id: "1344654704453484564" };
 // 自己紹介につけるやつ
-export const PROFILE_EMOJI = { name: "hude", id: "1355562102521336040" };
+export const PROFILE_EMOJI = { name: "blob_smile_happy_eyes", id: "1347190479162183733" };
 // 不参加用絵文字
-export const NON_PART = { name: "non_participation", id: "1388511196503867432" };
+export const NON_PART = { name: "non_participation", id: "1344654595992977489" };
 
 
 // チャンネルとメンションの組み合わせリスト
